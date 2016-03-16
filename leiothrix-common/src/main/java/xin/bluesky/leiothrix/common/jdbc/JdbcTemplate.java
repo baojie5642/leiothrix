d1acdd0dc69aa2b449b0758eb29211435e677cc9
张轲 <zhangke@datatrees.com.cn>
2016-03-16 15:15:42 +0800

    细节:变量定义的修改

--- leiothrix-common/src/main/java/xin/bluesky/leiothrix/common/jdbc/JdbcTemplate.java
+++ leiothrix-common/src/main/java/xin/bluesky/leiothrix/common/jdbc/JdbcTemplate.java
@@ -14,7 +14,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import static xin.bluesky.leiothrix.common.jdbc.JdbcUtils.closeQuietly;
 
